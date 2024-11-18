7462405242753ba0291b998906c78bc781da8cf5
Sergei <kondjansereg@mail.ru>
2024-11-18 20:00:55 +0700

    4. Утверждения с исключениями [#504886 #522257]

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -6,11 +6,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class NameLoadTest {
 
     @Test
-    void checkEmpty() {
+    void checkEmptyNames() {
         NameLoad nameLoad = new NameLoad();
-        assertThatThrownBy(nameLoad::getMap)
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("no data");
+        String[] names = new String[0];
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Names array is empty");
     }
 
     @Test
