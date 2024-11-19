814257fbc0453df8527f297c7f4417644b1aec56
Sergei <kondjansereg@mail.ru>
2024-11-19 19:21:06 +0700

    Уточнена директория сохранения результататов для ResultFile

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -4,7 +4,7 @@ import java.io.FileOutputStream;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        try (FileOutputStream out = new FileOutputStream("data/result.txt")) {
             int[][] rsl = new int[10][10];
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
