1114a75b657f43c04e51637d3aa4a211fd74d5dc
Sergei <kondjansereg@mail.ru>
2024-12-01 21:52:10 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,14 +8,30 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    public static void main(String[] args) throws IOException, IllegalArgumentException {
+        Search.chechForArgs(args);
+        Search.chechStartFolder(args[0]);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void chechForArgs(String[] args) throws IllegalArgumentException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Incorrect arguments! For search input "
+                    + "start folder and file extension");
+
+        }
+    }
+
+    private static void chechStartFolder(String arg) throws IllegalArgumentException {
+        if (!Files.isDirectory(Paths.get(arg))) {
+            throw new IllegalArgumentException("Incorrect start folder!");
+        }
     }
 }
